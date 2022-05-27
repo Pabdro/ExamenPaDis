77c5b2ff1d8d5c15ccd6b9a73433219e2b73fc9d
Pabdro <pablis.badani@gmail.com>
2022-05-27 09:11:48 -0400

    parcial1 final commit

---
+++ src/main/java/examen1/ej2/Client.java
@@ -0,0 +1,4 @@
+package examen1.ej2;
+
+public class Client {
+}
---
+++ src/main/java/examen1/ej2/ITelevisor.java
@@ -0,0 +1,7 @@
+package examen1.ej2;
+
+public interface ITelevisor extends Cloneable{
+    Object clone();
+}
+// clonable es una clase herarada a una interface no estabas manejando tipo de dato correcto
+// ya solucionado  , trabaja sigue aqui
